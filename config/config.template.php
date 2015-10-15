673133aee6794da26a10881385bdd04f394435a7
Rick Yust <ryust@efolder.net>
2015-10-15 12:49:14 -0600

    Add Providers, logging, and configuration.

---
+++ config/config.template.php
@@ -0,0 +1,7 @@
+<?php
+/** Template to be copied and filled in as config.local.php */
+return [
+    'dbname' => '',
+    'user' => '',
+    'password' => '',
+];
--- src/app.php
+++ src/app.php
@@ -4,8 +4,36 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/', function () {
-    return 'Here';
-});
+/** Debug toggle to see errors in browser */
+$app['debug'] = true;
+
+/** Local config values */
+list($dbname, $dbuser, $dbpass) = include_once(__DIR__ . '/../config/config.local.php');
+
+/** Register providers */
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+   'db.options' => array(
+       'driver' => 'pdo_mysql',
+       'dbname' => $dbname,
+       'host' => 'localhost',
+       'user' => $dbuser,
+       'password' => $dbpass,
+       'charset' => 'utf8',
+   )
+));
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__ . '/views',
+));
+$app->register(new Silex\Provider\MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../logs/dev.log',
+));
+
+
+/** Routes */
+$app->match('/', function () use ($app) {
+    $app['monolog']->addInfo('logging example');
+    return 'here';
+})
+->method('GET|POST');
 
 return $app;
